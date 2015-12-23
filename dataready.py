d753695f1c30d2ede444c59839ba95c5638afcd5
fukatani <nannyakannya@gmail.com>
2015-12-23 18:33:24 +0900

    add no dataready option

--- dataready.py
+++ dataready.py
@@ -16,7 +16,7 @@ def callback(pin):
     global write_file
     write_file.write(str(resp) + '\n')
 
-def int_communication(freq, debug=True):
+def int_communication(freq, no_data_ready):
     GPIO_DR = 6
     i = 0
     while os.path.exists('data_' + str(i) + '.dat'):
@@ -27,20 +27,21 @@ def int_communication(freq, debug=True):
     spi = spidev.SpiDev()
     spi.open(0,0)
     spi.max_speed_hz=(freq)
-    if debug: #for no dataready
-        callback(6)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(GPIO_DR, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(GPIO_DR, GPIO.RISING)
     GPIO.add_event_callback(GPIO_DR, callback)
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        GPIO.cleanup()
-        global write_file
-        write_file.close()
-        print('Written to data_' + str(i) + '.dat')
+    if no_data_ready:
+        callback(6)
+    else:
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            GPIO.cleanup()
+            global write_file
+            write_file.close()
+            print('Written to data_' + str(i) + '.dat')
 
 if __name__ == '__main__':
     optparser = OptionParser()
@@ -48,8 +49,9 @@ if __name__ == '__main__':
                          default=1000000, help="Spi frequency, Default=1000000")
     optparser.add_option("-c","--clean",dest="clean_flag",
                          default=False, help="Clean previous data or not, Default=False")
+    optparser.add_option("-n","--single",dest="single",
+                         default=False, help="Not wait data ready and execute single reading, Default=False")
     (options, args) = optparser.parse_args()
     if options.clean_flag:
         os.remove('data_0.txt')
-
-    int_communication(options.spi_freq, False)
+    int_communication(options.spi_freq, False, no_data_ready)
